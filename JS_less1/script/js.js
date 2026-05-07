9b80fb325cc886f771c9e2027ee5e22d9e6ec9c8
ksu_gorska <ksu.gorska@gmail.com>
2026-05-07 11:45:17 +0300

    Add files via upload

---
+++ JS_less1/script/js.js
@@ -0,0 +1,70 @@
+// document.write("Hello JS");
+// console.log('Hello World!');
+// console.info('Hello World2!');
+// console.error('Hello World3!');
+// console.warn('Hello World4!');
+
+// var MyFirstVar;
+// MyFirstVar = 10;
+// console.log(MyFirstVar);
+//
+// var MySecondVar = "23";
+// console.log(MySecondVar + MyFirstVar);
+// console.log(MySecondVar - MyFirstVar);
+//
+// var int =  10;
+// var float = 12.5;
+// var bool = true;//false
+// var str = "6";
+// var a;
+// a = null;
+// console.log(a);
+
+// let b = 10, a = b + 2;
+
+// a = 10
+// let a;
+//
+// console.log(a);
+
+// let a = 10;
+// a = 20;
+// console.log(a)
+//
+// const P1 = 3.14;
+
+// let a = 10, b = 3;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+// a = 2 + 2;
+// b = a + 10;
+//
+// a = a + 2;
+// a += 2;
+// a /= 2;
+
+// a++; //incremen syfics
+// a--; //decremen
+//
+// --a; //prefics
+// ++a;
+//
+// let a = 2, b;
+// b = ++a;
+// console.log(b);
+//
+// let a = '3', b = '5';
+// console.log((a - 0) + (b - 0));
+//
+// console.log(Number(a) - Number(b));
+
+//interpoliacia
+// let name = "ksu", age = 15;
+// console.log("Привіт " + name + "Тобі" + age + "років");
+// console.log(`Привіт ${name}. Тобі ${age} років ${((a - 0) + (b - 0))}`);
+
