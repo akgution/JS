1262597ba8f1aba1b83cf0ed481880f5cca24802
ksu_gorska <ksu.gorska@gmail.com>
2026-05-19 13:45:47 +0300

    Add files via upload

---
+++ less3_cycle/script/js.js
@@ -0,0 +1,72 @@
+// for(var i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+// let n = prompt("Number: ");
+// for (let i = 1; i <= n; i++) {
+//    if (i % 2 == 0) {
+//        console.log(i);
+//    }
+// }
+
+// let n = prompt("Number: ");
+// suma = 0
+// for (let i = 1; i <= n; i++) {
+//     suma += i
+// }
+// console.log(suma);
+
+// let n = prompt("Number: ");
+// for (let i = n; i > 0; i--) {
+//     console.log(i)
+// }
+
+// let n = prompt("Number : "), mn;
+// for (let i = 1; i <= 10; i++) {
+//     mn =  n * i
+//     console.log(`${n} * ${i} = ${mn}`);
+// }
+
+// let ps;
+// do{
+//     ps = prompt("Password : ")
+//     if (ps !== "admin123") {
+//         console.log("Incorrect password")
+//     }
+// }while(ps !== "admin123");
+// console.log("Correct password")
+
+// let pscr = "admin123", ps;
+// for (let i = 1; i <= 3; i++) {
+//     ps = prompt("Password : ")
+//     if (i === 3) {
+//            alert("No dostup");
+//        }
+//     if (ps === pscr){
+//         alert("Correct");
+//         break;
+//     } else{
+//         alert("Wrong Password");
+//     }
+// }
+
+// for(var i = 1; i <= 20; i++){
+//     if (i % 2 == 0) continue;
+//     console.log(i);
+// }
+
+// let n = prompt("Number : ");
+// console.log(n.length);
+
+// let i = 10, n = prompt("Guess Number : ");
+// while (i === n) {
+//     if (n > i) {
+//         alert(">");
+//     }
+//     if (n < i) {
+//         alert("<");
+//     }
+//     else{
+//         alert("Vgadano");
+//     }
+// }
